b891925fd9ef9a87313c890820eaf613ac967fa9
Jay Lewis <jr_lewis@outlook.com>
2020-04-01 20:13:42 +0100

    Setting up contact link in navbar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
@@ -7,28 +7,28 @@ const port = process.env.PORT || 5000;
 
 const uri = "mongodb://dirtAdmin:admin123@ds247047.mlab.com:47047/video-sources";
 
-mongoose.connect(
-  uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
-const connection = mongoose.connection;
+// mongoose.connect(
+//   uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }
+// )
+// const connection = mongoose.connection;
 
-connection.once("open", () => {
-  console.log("MongoDB database connection successful");
-})
+// connection.once("open", () => {
+//   console.log("MongoDB database connection successful");
+// })
 
-app.get("/fetch", function (req, res) {
-  const collection = db.collection("video-sources");
-  collection.find({}, function (err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  })
-});
+// app.get("/fetch", function (req, res) {
+//   const collection = db.collection("video-sources");
+//   collection.find({}, function (err, result) {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.send(result);
+//     }
+//   })
+// });
 
 
 app.use(express.static(__dirname + "/public"));
@@ -39,12 +39,4 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
-})
-
-app.get("/contact", (req, res) => {
-  res.sendFile(__dirname + "/public/contact.html")
-})
-
-app.get("/slick", (req, res) => {
-  res.sendFile(__dirname + "/slick.html")
 })
